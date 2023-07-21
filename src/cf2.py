7e9540ba85bb03f83ba50cac3521cf2bb2da858d
Nick Lindsay <23167353+NicholasLindsay@users.noreply.github.com>
2023-07-20 20:03:25 -0400

    refactoring, enable type checking

--- src/cf2.py
+++ src/cf2.py
@@ -7,13 +7,12 @@ The intended usecase is to enable reproducible experiments by bringing
 the system into a standardized environent.
 """
 
-from typing import Any
+from typing import Any, Optional
 
 class MetaTreeNode:
     __name: str
     __helpstring: str
-    __parent: 'Optional[MetaTreeNode]'
-    __children: dict[str, 'MetaTreeNode']
+    __parent: 'Optional[MetaTreeFixedDict]'
 
     def __init__(self, name: str, helpstring: str, **kwargs):
         self.__name = name
@@ -21,8 +20,7 @@ class MetaTreeNode:
         self.__parent = None
         if 'parent' in kwargs:
             self.__parent = kwargs['parent']
-            self.__parent.__RegisterChild(self)
-        self.__children = {}
+            self.__parent.RegisterChild(self)
 
     def Name(self) -> str:
         return self.__name
@@ -30,23 +28,94 @@ class MetaTreeNode:
     def HelpString(self) -> str:
         return self.__helpstring
 
-    def Parent(self) -> 'MetaTreeNode':
+    def Parent(self) -> 'Optional[MetaTreeNode]':
         return self.__parent
     
+    def IsScalar(self) -> bool:
+        return False
+    
+    def TypeString(self) -> str:
+        return type(self).__name__
+
+    def MetadataTreeAsStr(self, is_top=True, is_last_sibling=True):
+        s = ''
+        if is_top:
+            s = f'{self.Name()}: {self.HelpString()}\n'
+        else:
+            if is_last_sibling:
+                s = f' └── {self.Name()}: {self.HelpString()}\n'
+            else:
+                s = f' ├── {self.Name()}: {self.HelpString()}\n'
+            
+        return s
+
+    def _TypeCheckRecursive(self, path: list[str], data: Any) -> list[str]:
+        return []
+    
+class MetaTreeFixedDict(MetaTreeNode):
+    __children: dict[str, 'MetaTreeNode']
+
+    def __init__(self, name: str, helpstring: str, **kwargs):
+        super().__init__(name, helpstring, **kwargs)
+        self.__children = {}
+
     def Children(self) -> 'dict[str, MetaTreeNode]':
         return self.__children
-    
+
     def IsLeaf(self) -> bool:
         return (not bool(self.Children()))
     
     def IsScalar(self) -> bool:
         return False
     
+    def TypeString(self) -> str:
+        return "FixedDict"
+
     def __getitem__(self, key: str) -> 'MetaTreeNode':
         return self.__children[key]
 
-    def __RegisterChild(self, ch: 'MetaTreeNode'):
+    def RegisterChild(self, ch: 'MetaTreeNode'):
+        assert(ch.Name() not in self.__children)
         self.__children[ch.Name()] = ch
+
+    def MetadataTreeAsStr(self, is_top=True, is_last_sibling=True):
+        s = super().MetadataTreeAsStr(is_top, is_last_sibling)
+
+        prefix = ''
+        if not is_top:
+            if is_last_sibling:
+                prefix = '    '
+            else:
+                prefix = ' │  '
+
+        children_list = list(self.Children().values())
+        for c in children_list:
+            if c == children_list[-1]:
+                s += PrefixLines(c.MetadataTreeAsStr(False, True), prefix)
+            else:
+                s += PrefixLines(c.MetadataTreeAsStr(False, False), prefix)
+            
+        return s
+
+    def _TypeCheckRecursive(self, path: list[str], data: Any) -> list[str]:
+        errlist: list[str] = []
+        pathstr = '.'.join(path)
+
+        if type(data) is not dict:
+            errlist.append(f"{pathstr}: type mismatch (expected: dict got: {type(data).__name__})")
+        else:
+            for k in data:
+                if k not in self.Children():
+                    errlist.append(f"{pathstr}: \"{k}\" is not a valid key")
+                else:
+                    errlist += self[k]._TypeCheckRecursive((path + [k]), data[k])
+
+            for k in self.Children():
+                if k not in data:
+                    typetext = self[k].TypeString()
+                    errlist.append(f"{pathstr}: missing \"{k}\" field [Type = {typetext}]")
+
+        return errlist
 
 class MetaTreeScalar(MetaTreeNode):
     __ty: type
@@ -64,42 +133,30 @@ class MetaTreeScalar(MetaTreeNode):
     
     def IsScalar(self) -> bool:
         return True
+    
+    def IsLeaf(self) -> bool:
+        return False
+    
+    def TypeString(self) -> str:
+        return self.Ty().__name__
+
+    def _TypeCheckRecursive(self, path: list[str], data: Any) -> list[str]:
+        errlist: list[str] = []
+        pathstr = '.'.join(path)
+
+        if type(data) != self.Ty():
+            errlist.append(f"{pathstr}: type mismatch (expected: {self.TypeString()} got: {type(data).__name__})")
+
+        return errlist
 
 # Helper function to insert text at start of each line
 def PrefixLines(s: str, prefix: str):
     new_lines = [f'{prefix}{l}' for l in s.splitlines(keepends=True)]
     return ''.join(new_lines)
 
-def MetadataTreeAsStr(root: MetaTreeNode, is_top=True, is_last_sibling=True):
-    s = ''
-    if is_top:
-        s = f'{root.Name()}: {root.HelpString()}\n'
-    else:
-        if is_last_sibling:
-            s = f' └── {root.Name()}: {root.HelpString()}\n'
-        else:
-            s = f' ├── {root.Name()}: {root.HelpString()}\n'
-
-    prefix = ''
-    if not is_top:
-        if is_last_sibling:
-            prefix = '    '
-        else:
-            prefix = ' |  '
-
-    children_list = list(root.Children().values())
-
-    for c in children_list:
-        if c == children_list[-1]:
-            s += PrefixLines(MetadataTreeAsStr(c, False, True), prefix)
-        else:
-            s += PrefixLines(MetadataTreeAsStr(c, False, False), prefix)
-        
-    return s
-
 # ===[ META MODEL DEFINITION ]===
-top = MetaTreeNode("top", "top node")
-ksm = MetaTreeNode("ksm", "kernel samepage merging", parent=top)
+top = MetaTreeFixedDict("top", "top node")
+ksm = MetaTreeFixedDict("ksm", "kernel samepage merging", parent=top)
 MetaTreeScalar("max_page_sharing", "", int, parent=ksm)
 MetaTreeScalar("merge_accross_nodes", "", int, parent=ksm)
 MetaTreeScalar("pages_to_scan", "", int, parent=ksm)
@@ -107,18 +164,18 @@ MetaTreeScalar("run", "", int, parent=ksm)
 MetaTreeScalar("sleep_millisecs", "", int, parent=ksm)
 MetaTreeScalar("stable_node_chains_prune_millisecs", "", int, parent=ksm)
 MetaTreeScalar("use_zero_pages", "", int, parent=ksm)
-lru_gen = MetaTreeNode("lru_gen", "", parent=top)
+lru_gen = MetaTreeFixedDict("lru_gen", "", parent=top)
 MetaTreeScalar("enabled", "", str, parent=lru_gen)
 MetaTreeScalar("min_ttl_ms", "", int, parent=lru_gen)
-numa = MetaTreeNode("numa", "non-uniform memory access", parent = top)
+numa = MetaTreeFixedDict("numa", "non-uniform memory access", parent = top)
 MetaTreeScalar("demotion_enabled", "", bool, parent=numa)
-swap = MetaTreeNode("swap", "", parent=top)
+swap = MetaTreeFixedDict("swap", "", parent=top)
 MetaTreeScalar("vma_ra_enabled", "", bool, parent=swap)
-thp = MetaTreeNode("transparent_hugepage", "transparent hugepages", parent=top)
+thp = MetaTreeFixedDict("transparent_hugepage", "transparent hugepages", parent=top)
 MetaTreeScalar("defrag", "", str, parent=thp)
 MetaTreeScalar("enabled", "", str, parent=thp)
 MetaTreeScalar("hpage_pmd_size", "", int, parent=thp)
-khpd = MetaTreeNode("khugepaged", "huge pages daemon", parent=thp)
+khpd = MetaTreeFixedDict("khugepaged", "huge pages daemon", parent=thp)
 MetaTreeScalar("alloc_sleep_millisecs", "", int, parent=khpd)
 MetaTreeScalar("max_ptes_none", "", int, parent=khpd)
 MetaTreeScalar("max_ptes_shared", "", int, parent=khpd)
@@ -139,38 +196,9 @@ class MetaModel:
         return self.__root
 
     def TreeAsString(self) -> str:
-        return MetadataTreeAsStr(self.Root())
+        return self.__root.MetadataTreeAsStr()
+    
+    def TypeCheck(self, data) -> list[str]:
+        return self.Root()._TypeCheckRecursive([], data)
 
-standard_metamodel = MetaModel(top)
-# print(standard_metamodel.TreeAsString())
-
-def _TypeCheckRecursive(path: list[str], data: Any, meta: MetaTreeNode) -> list[str]:
-    errlist: list[str] = []
-    pathstr = '.'.join(path)
-
-    if meta.IsScalar():
-        assert(meta.IsLeaf())
-        if type(data) != meta.Ty():
-            errlist.append(f"{pathstr}: type mismatch (expected: {meta.Ty().__name__} got: {type(data).__name__})")
-    else:
-        assert(not meta.IsLeaf())
-        
-        if type(data) is not dict:
-            errlist.append(f"{pathstr}: type mismatch (expected: dict got: {type(data).__name__})")
-        else:
-            for k in data:
-                if k not in meta.Children():
-                    errlist.append(f"{pathstr}: \"{k}\" is not a valid key")
-                else:
-                    errlist += _TypeCheckRecursive((path + [k]), data[k], meta[k])
-
-            for k in meta.Children():
-                if k not in data:
-                    typetext = meta[k].Ty().__name__ if meta[k].IsScalar() else "dict"
-                    errlist.append(f"{pathstr}: missing \"{k}\" field [Type = {typetext}]")
-
-    return errlist
-
-def TypeCheck(data: tuple[str, Any], model: MetaModel) -> list[str]:
-    """Typecheck data against model, return list of errors OR empty list if OK"""
-    return _TypeCheckRecursive([model.Root().Name()], data, model.Root())
+STANDARD_METAMODEL = MetaModel(top)
--- tests/test_typecheck.py
+++ tests/test_typecheck.py
@@ -5,12 +5,12 @@ import unittest
 from src.cf2 import *
 
 # ===[ Simple MetaModel used by tests ]===
-TOP = MetaTreeNode("top", "i am top")
+TOP = MetaTreeFixedDict("top", "i am top")
 BAR = MetaTreeScalar("bar", "i am bar", int, parent=TOP)
-BAZ = MetaTreeNode("baz", "i am baz", parent=TOP)
+BAZ = MetaTreeFixedDict("baz", "i am baz", parent=TOP)
 MetaTreeScalar("name", "person's name", str, parent=BAZ)
 MetaTreeScalar("age", "person's age", int, parent=BAZ)
-TEAMS = MetaTreeNode("teams", "sports teams", parent=TOP)
+TEAMS = MetaTreeFixedDict("teams", "sports teams", parent=TOP)
 MetaTreeScalar("soccer", "soccer", str, parent=TEAMS)
 MetaTreeScalar("nfl", "american football", str, parent=TEAMS)
 TEST_METAMODEL = MetaModel(TOP)
@@ -19,36 +19,32 @@ TEST_METAMODEL = MetaModel(TOP)
 class TestTypeCheck(unittest.TestCase):
     def test_good_case(self):
         good_data = {
-                        "foo": {
-                            "bar": 5,
-                            "baz": {
-                                "name": "john",
-                                "age": 37,
-                            },
-                            "teams" : {
-                                "soccer": "man utd",
-                                "nfl": "tigers",
-                            }
+                        "bar": 5,
+                        "baz": {
+                            "name": "john",
+                            "age": 37,
+                        },
+                        "teams" : {
+                            "soccer": "man utd",
+                            "nfl": "tigers",
                         }
                     }
-        errlist = TypeCheck(good_data["foo"], TEST_METAMODEL)
-        self.assertEqual(len(errlist), 0, "detected error in good case")
+        errlist = TEST_METAMODEL.TypeCheck(good_data)
+        self.assertEqual(errlist, [], "detected error in good case")
 
     def test_bad_case(self):
         bad_data = {
-                        "foo" : {
-                            "bar" : False,
-                            "baz" : {
-                                "age" : "100",
-                                "hobby" : [ "fishing" , "eating" ]
-                            },
-                            "teams": 619,
-                            "etc" : {
-                                "phone" : 123456709,
-                            }
+                        "bar" : False,
+                        "baz" : {
+                            "age" : "100",
+                            "hobby" : [ "fishing" , "eating" ]
+                        },
+                        "teams": 619,
+                        "etc" : {
+                            "phone" : 123456709,
                         }
                     }
-        errlist = TypeCheck(bad_data["foo"], TEST_METAMODEL)
+        errlist = TEST_METAMODEL.TypeCheck(bad_data)
         self.assertNotEqual(len(errlist), 0)
 
 # ===[ Boilerplate ]===
